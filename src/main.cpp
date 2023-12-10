1b6bfd6d2c1f28f85c8854a021ef01c24cc9605a
Noah Zhou <noahzhou@Noahs-MBP.lan>
2023-12-10 15:47:06 -0800

    added accurateRotation with imu sensor instead of weaker okapilib 2 motor odometry

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "main.h" //definitions
 #include "autoSelect/selection.h"
-Motor rightwheel(19);
-Motor leftwheel(20);
+pros::Motor rightwheel(19);
+pros::Motor leftwheel(20);
 Motor elevate1(6);
 Motor elevate2(7); // reversed direction
 Motor intake(13);
@@ -9,6 +9,7 @@ Motor flywheel(12);
 Motor flywheel1(11); // reversed direction
 Controller master(E_CONTROLLER_MASTER);
 bool pressed = false;
+pros::Imu imu_sensor(5); // temp imu init
 /**
  * A callback function for LLEMU's center button.
  *
@@ -37,6 +38,16 @@ void initialize()
 	// pros::lcd::set_text(2, "test");
 
 	// pros::lcd::register_btn1_cb(on_center_button);
+	int time = pros::millis();
+	int iter = 0;
+	while (imu_sensor.is_calibrating())
+	{
+		printf("IMU calibrating... %d\n", iter);
+		iter += 10;
+		pros::delay(10);
+	}
+	printf("IMU is done calibrating (took %d ms)\n", iter - time);
+	pros::delay(1000);
 	selector::init();
 }
 
@@ -70,6 +81,7 @@ void disabled()
  */
 void competition_initialize()
 {
+
 	elevate1.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD); // change brake mode to hold mode: dynamic braking with a PID loop to hold the motor in place so when you release elevate button it stayas in place
 	elevate2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 }
@@ -85,7 +97,28 @@ void competition_initialize()
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-
+float accurateRotation(float target){
+	// using namespace pros;
+	imu_sensor.tare_rotation(); //IMU_PORT
+	double current = imu_sensor.get_rotation();
+	double target = target;
+	double tolerance = 0.5; //deg tolerance
+	while (current != target){
+		if (current > target-tolerance){
+			rightwheel.move_velocity(50);
+			leftwheel.move_velocity(-50);
+		}
+		else if (current < target+tolerance){
+			rightwheel.move_velocity(-50);
+			leftwheel.move_velocity(50);
+		}
+		else{
+			rightwheel.move_velocity(0);
+			leftwheel.move_velocity(0);
+		}
+		current = imu_sensor.get_rotation();
+	}
+}
 void autonomous()
 {
 	/**
@@ -114,27 +147,28 @@ void autonomous()
 		drive->setMaxVelocity(200);
 		drive->moveDistance(10_in);
 	}
-	else if(selector::auton == 2){ //run auton for Back Red 
-	drive->moveDistance(22_in); // move 22 inches forward; float value
+	else if (selector::auton == 2)
+	{								// run auton for Back Red
+		drive->moveDistance(22_in); // move 22 inches forward; float value
 		drive->turnAngle(90_deg);
 	}
 	else if (selector::auton == -1)
 	{								// run auton for Front Blue
 		drive->moveDistance(22_in); // move 22 inches forward
-		drive->turnAngle(-90_deg);	
+		drive->turnAngle(-90_deg);
 		drive->setMaxVelocity(200);
 		drive->moveDistance(10_in);
 		for (int i = 0; i < 5; i++)
 		{
 			drive->moveDistanceAsync(-3_in);
 			drive->moveDistanceAsync(3_in);
-
 		}
 	}
-	else if(selector::auton == -2){ //run auton for Back Blue; commented out because throws error if else block is empty 
-	drive->moveDistance(22_in); // move 22 inches forward
+	else if (selector::auton == -2)
+	{								// run auton for Back Blue; commented out because throws error if else block is empty
+		drive->moveDistance(22_in); // move 22 inches forward
 		drive->turnAngle(-90_deg);
-		}
+	}
 	// else if(selector::auton == 0){ //run auton for Skills }
 	// else
 	{ // do nothing }
@@ -170,7 +204,7 @@ void opcontrol()
 	flywheel.set_brake_mode(pros::E_MOTOR_BRAKE_COAST); // lower chance of motor burning out with coasting flywheel
 	flywheel1.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	double deadzone = master.get_analog(ANALOG_LEFT_Y); // deadzone for left joystick
-	
+
 	while (true)
 	{
 		rightx = master.get_analog(ANALOG_LEFT_X);
@@ -180,7 +214,7 @@ void opcontrol()
 		// pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		//                  (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		//                  (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0); // debug functions
-		if (master.get_digital(DIGITAL_LEFT))	
+		if (master.get_digital(DIGITAL_LEFT))
 		{
 			pressed = true;
 		}
@@ -188,21 +222,23 @@ void opcontrol()
 		{
 			pressed = false;
 		}
-		if (pressed == false){
-			
-		rightwheel = righty + rightx; // arcade drivemode, reverse one motor direction so it drives normally
-		leftwheel = rightx - righty;	
+		if (pressed == false)
+		{
+
+			rightwheel = righty + rightx; // arcade drivemode, reverse one motor direction so it drives normally
+			leftwheel = rightx - righty;
 		}
-		else if (pressed == true){
-			rightwheel = -righty+rightx;
-			leftwheel = righty+rightx;
+		else if (pressed == true)
+		{
+			rightwheel = -righty + rightx;
+			leftwheel = righty + rightx;
 		}
-		if (lefty != 0; lefty>=7 || lefty<=-7)
+		if (lefty != 0; lefty >= 7 || lefty <= -7)
 		{
 			flywheel.move_velocity(-lefty);
 			flywheel1.move_velocity(lefty);
 		}
-		
+
 		if (master.get_digital(DIGITAL_UP))
 		{
 			elevate1 = 127;
